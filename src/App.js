a094e4146ed7b1caa75a9b7bb50535a4167a92ef
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-11-08 19:51:04 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Post from './Post';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app__header">
+        <img className="app__headerImage" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png" alt=""/>
+      </div>
+      {/* Header */}
+
+      {/* Post */}
+      {/* Post */}
     </div>
   );
 }
