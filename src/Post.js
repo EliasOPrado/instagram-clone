ebdc70157eeb0b5b2d5d2b94f47d677109a9b066
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-11-08 20:25:45 +0000

    added avatar

--- src/Post.js
+++ src/Post.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import './Post.css'
+import Avatar from "@material-ui/core/Avatar";
 
 function Post() {
     return (
-        <div>
+        <div className="post">
+            <div className="post__header">
+            <Avatar 
+            className="post__avatar"
+            alt="elias"
+            src="/static/images/avatar/1.jpg"
+            />
             <h3>Username</h3>
+            </div>
             {/* header -> avatar + username */}
-            {/* image */}
-            {/* username + caption */}
+            <img className="post__image" src="https://cdn.auth0.com/blog/illustrations/reactjs.png" alt=""/>
+
+            <h4 className="post__text"><strong>Elias:</strong> Criando instagram clone usando react..</h4>
         </div>
     )
 }
